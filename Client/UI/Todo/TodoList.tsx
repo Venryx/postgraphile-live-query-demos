8d21a618118bff235bcddd3ced665d7fdffbf188
Venryx <venryx@gmail.com>
2021-05-15 17:23:08 -0700

    * MS todo entries have button to randomize their text. (useful for making a mutation, but keeping same list/content size, for comparison of payloads)

--- Client/UI/Todo/TodoList.tsx
+++ Client/UI/Todo/TodoList.tsx
@@ -5,22 +5,33 @@ import {Row} from "../../Utils/ReactComponents/Row";
 import {TodoEntry} from "./@TodoEntry";
 
 const GET_TODO_ENTRIES = gql`
-	subscription GET_TODO_ENTRIES {
-		todoEntries {
-			nodes {
-				id
-				createdAt
-				text
-			}
+subscription GET_TODO_ENTRIES {
+	todoEntries {
+		nodes {
+			id
+			createdAt
+			text
 		}
 	}
+}
 `;
 const CREATE_TODO_ENTRY = gql`
-	mutation CREATE_TODO_ENTRY($entry: TodoEntryInput!) {
-		createTodoEntry(input: {todoEntry: $entry}) {
-			clientMutationId
+mutation CREATE_TODO_ENTRY($entry: TodoEntryInput!) {
+	createTodoEntry(input: {todoEntry: $entry}) {
+		clientMutationId
+	}
+}
+`;
+export const UPDATE_TODO_ENTRY = gql`
+mutation UPDATE_TODO_ENTRY($id: String!, $patch: TodoEntryPatch!) {
+	updateTodoEntry(input: {id: $id, patch: $patch}) {
+		todoEntry {
+			id
+			createdAt
+			text
 		}
 	}
+}
 `;
 const DELETE_TODO_ENTRY = gql`
 mutation DELETE_TODO_ENTRY($id: String!) {
@@ -34,6 +45,7 @@ export function TodoList() {
 	//const {loading, error, data: {todoEntries: {nodes: entries = []} = {}} = {}} = useSubscription(GET_TODO_ENTRIES);
 	const {loading, error, data: data1} = useSubscription(GET_TODO_ENTRIES);
 	const [addTodoEntry, {data: data2}] = useMutation(CREATE_TODO_ENTRY);
+	const [updateTodoEntry, info] = useMutation(UPDATE_TODO_ENTRY);
 	const [deleteTodoEntry, {}] = useMutation(DELETE_TODO_ENTRY);
 	let [newEntryText, setNewEntryText] = useState("");
 	
@@ -42,7 +54,7 @@ export function TodoList() {
 	
 	const entries = data1?.todoEntries?.nodes ?? [];
 	return (
-		<Column>
+		<Column style={{width: 300}}>
 			<Row>
 				<input type="text" value={newEntryText} onChange={e=>{
 					setNewEntryText(e.target.value);
@@ -56,13 +68,21 @@ export function TodoList() {
 			{entries.map((entry, index)=>{
 				return (
 					<Row key={index}>
-						<span>{entry.text}</span>
+						<span style={{flex: 1}}>{entry.text}</span>
 						{/*<button disabled={index == 0} onClick={()=>{
 							// todo
 						}}>Up</button>
 						<button disabled={index == entries.length - 1} onClick={()=>{
 							// todo
 						}}>Dn</button>*/}
+						<button onClick={()=>{
+							updateTodoEntry({variables: {
+								id: entry.id,
+								patch: {
+									text: "Random_" + Math.random(),
+								},
+							}});
+						}}>Rnd</button>
 						<button onClick={()=>{
 							deleteTodoEntry({variables: {id: entry.id}});
 						}}>X</button>
