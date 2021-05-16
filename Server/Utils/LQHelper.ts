f7980bfc9a1ddedb8097000bdf027db58b2a49d8
Venryx <venryx@gmail.com>
2021-05-16 00:20:07 -0700

    * Tried using lower-level `builder.hook("GraphQLObjectType:fields:field", [...])`, but that has the same issue, of its return value trying to be "resolved" to subfields.
    Next: Try `GraphQLObjectType:fields`.

--- Server/Utils/LQHelper.ts
+++ Server/Utils/LQHelper.ts
@@ -15,88 +15,132 @@ const {makeWrapResolversPlugin} = PGUtils;
 	},
 });*/
 // Example: log before and after each mutation runs
-export default makeWrapResolversPlugin(
-	context => {
-		//if (context.scope.isRootMutation) {
-		if (context.scope.isRootQuery) {
-			return {scope: context.scope};
-		}
-		return null;
-	},
-	({scope}) => async (resolver, user, args, context, _resolveInfo) => {
-		const result = await resolver(user, args, context, _resolveInfo);
+export default function(builder) {
+	builder.hook("GraphQLObjectType:fields:field", (field, build, context)=>{
+		const {pgSql: sql} = build;
+		const { scope: { isRootQuery, isRootSubscription, fieldName }, addArgDataGenerator } = context;
 
-		/*global["_lastClientID"] ??= 1;
-		context._clientID ??= ++global["_lastClientID"];*/
-		//context._clientID ??= global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
-		//context._clientID ??= global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
-		if (context._clientID == null) {
-			context._clientID = global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
-			const patchGenerator = createLiveQueryPatchGenerator();
-			context._patchGenerator_queue = [];
-			context._patchGenerator_queue[Symbol.asyncIterator] = function*() {
-				while (true) {
-					const nextEntry = context._patchGenerator_queue[0];
-					yield {...nextEntry, isLive: true};
-				}
-			};
-			context._patchGenerator_call = patchGenerator(context._patchGenerator_queue)
+		const isMatch = (isRootQuery || isRootSubscription) && fieldName == "todoEntries"
+		if (!isMatch) {
+			// The 'GraphQLObjectType:fields:field' hook runs for every field on
+			// every object type in the schema. If it's not a field in the root
+			// mutation type, or the field isn't named 'createLink', we don't want
+			// to modify it in this hook - so return the input object unmodified.
+			return field;
 		}
-		console.log("Client ID:", context._clientID);
 
-		//console.log(`Query. Info:`, {resolver, user, args, context, _resolveInfo});
-		/* [example log for subscription op]
-			{ [...]
-				_resolveInfo: { [...]
-					rootValue: {
-						liveAbort: [Function: liveAbort],
-						counter: 0,
-						liveCollection: [Function: bound liveCollection],
-						liveRecord: [Function: bound liveRecord],
-						liveConditions: [],
-						release: [Function: release]
+		// We're going to need link.id for our `performAnotherTask`; so we're going
+		// to abuse addArgDataGenerator to make sure that this field is ALWAYS
+		// requested, even if the user doesn't specify it. We're careful to alias
+		// the result to a field that begins with `__` as that's forbidden by
+		// GraphQL and thus cannot clash with a user's fields.
+		addArgDataGenerator(() => ({
+			pgQuery: queryBuilder => {
+			queryBuilder.select(
+				// Select this value from the result of the INSERT:
+				sql.query`${queryBuilder.getTableAlias()}.id`,
+				// And give it this name in the result data:
+				"__createdRecordId"
+			);
+			},
+		}));
+
+		// It's possible that `resolve` isn't specified on a field, so in that case
+		// we fall back to a default resolver.
+		const defaultResolver = obj => obj[fieldName];
+
+		// Extract the old resolver from `field`
+		const { resolve: oldResolve = defaultResolver, ...rest } = field;
+
+		return {
+			// Copy over everything except 'resolve'
+			...rest,
+
+			// Add our new resolver which wraps the old resolver
+			async resolve(...resolveParams) {
+				const [source, args, gqlContext, resolveInfo] = resolveParams;
+
+				// Perform some validation (or any other action you want to do before
+				// calling the old resolver)
+				/*const RESOLVE_ARGS_INDEX = 1;
+				const {
+					input: {
+						link: { title },
 					},
-					operation: {
-						kind: 'OperationDefinition',
-						operation: 'subscription',
-						name: { kind: 'Name', value: 'GET_TODO_ENTRIES', loc: [Object] },
-						variableDefinitions: [],
-						directives: [],
-						selectionSet: { kind: 'SelectionSet', selections: [Array], loc: [Object] },
-						loc: { start: 0, end: 139 }
-					}, [...]
-				} [...]
-			}
-		*/
-		
-		//if (_resolveInfo.parentType.name == "Subscription") {
-		if (_resolveInfo.operation.operation == "subscription") {
-			//console.log("\nOp:", _resolveInfo.operation);
-			console.log(`Result:`, result, "isAsyncIter:", isAsyncIterable(result));
+				} = resolveParams[RESOLVE_ARGS_INDEX];
+				if (title.length < 3) {
+					throw new Error("Title is too short!");
+				}*/
 
-			/*if (isAsyncIterable(result)) {
-				const resultAsPatch = context._makePatch(result);
-				console.log(`FullResult:`, result, "ResultAsPatch:", resultAsPatch);
-				return resultAsPatch;
-			}
-			return result;*/
+				// Call the old resolver (you SHOULD NOT modify the arguments it
+				// receives unless you also manipulate the AST it gets passed as the
+				// 4th argument; which is quite a lot of effort) and store the result.
+				const oldResolveResult = await oldResolve(...resolveParams);
 
-			//context._patchGenerator_queue.length = 0;
-			context._patchGenerator_queue[0] = result;
-			const resultAsPatch = (await context._patchGenerator_call.next()).value;
+				// custom section
+				// ==========
 
-			// if data field is null (ie. result is actually a patch, not the initial results)
-			if (resultAsPatch.data == null) {
-				debugger;
-				resultAsPatch.data = []; // add an empty "data" prop, else PgTablesPlugin.js errors
-			}
+				console.log(`Query. Info:`, {
+					field, build, context,
+					source, args, gqlContext, resolveInfo,
+				});
 
-			console.log(`FullResult:`, result, "ResultAsPatch:", resultAsPatch);
-			return resultAsPatch;
-		}
-		return result;
-	}
-);
+				/*global["_lastClientID"] ??= 1;
+				gqlContext._clientID ??= ++global["_lastClientID"];*/
+				//gqlContext._clientID ??= global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
+				//gqlContext._clientID ??= global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
+				if (gqlContext._clientID == null) {
+					gqlContext._clientID = global["_lastClientID"] = (global["_lastClientID"]|0) + 1;
+					const patchGenerator = createLiveQueryPatchGenerator();
+					gqlContext._patchGenerator_queue = [];
+					gqlContext._patchGenerator_queue[Symbol.asyncIterator] = function*() {
+						while (true) {
+							const nextEntry = gqlContext._patchGenerator_queue[0];
+							yield {...nextEntry, isLive: true};
+						}
+					};
+					gqlContext._patchGenerator_call = patchGenerator(gqlContext._patchGenerator_queue)
+				}
+				console.log("Client ID:", gqlContext._clientID);
+
+				//if (_resolveInfo.parentType.name == "Subscription") {
+				//if (_resolveInfo.operation.operation == "subscription") {
+				if (true) {
+					//console.log("\nOp:", _resolveInfo.operation);
+					console.log(`Result:`, oldResolveResult, "isAsyncIter:", isAsyncIterable(oldResolveResult));
+
+					/*if (isAsyncIterable(result)) {
+						const resultAsPatch = gqlContext._makePatch(result);
+						console.log(`FullResult:`, result, "ResultAsPatch:", resultAsPatch);
+						return resultAsPatch;
+					}
+					return result;*/
+
+					//gqlContext._patchGenerator_queue.length = 0;
+					gqlContext._patchGenerator_queue[0] = oldResolveResult;
+					const resultAsPatch = (await gqlContext._patchGenerator_call.next()).value;
+
+					// if empty patch, it means client is just re-requesting the full/orig list, so return that
+					if (resultAsPatch.patch && resultAsPatch.patch.length == 0) {
+						return oldResolveResult;
+					}
+
+					// if data field is null (ie. result is actually a patch, not the initial results)
+					if (resultAsPatch.data == null) {
+						debugger;
+						resultAsPatch.data = []; // add an empty "data" prop, else PgTablesPlugin.js errors
+					}
+
+					console.log(`FullResult:`, oldResolveResult, "ResultAsPatch:", resultAsPatch);
+					return resultAsPatch;
+				}
+
+				// Finally return the result.
+				return oldResolveResult;
+			},
+		};
+	});
+}
 
 const isAsyncIterable = (value: unknown): value is AsyncIterable<unknown> => {
 	return (
